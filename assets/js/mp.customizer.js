6a330677b94bfd6896c4bdca23fc3457a3eb8192
Ade Viankakrisna <viankakrisna@gmail.com>
2016-02-11 00:56:33 +0700

    add chrome header color function

--- assets/js/mp.customizer.js
+++ assets/js/mp.customizer.js
@@ -53,6 +53,9 @@ MP.customizer = (function ($) {
             $('#theme-link')
                 .attr('href', 'assets/css/material.' + theme + '.min.css');
             storage.set(setting);
+            $('[name="theme-color"]')
+                .attr('content', colorToHex($('header')
+                    .css('background-color')));
             $theme.val(theme);
         }
     }
@@ -64,6 +67,18 @@ MP.customizer = (function ($) {
         $('#font')
             .val('Roboto')
             .trigger('change');
+    }
+
+    function colorToHex(color) {
+        if (color.substr(0, 1) === '#') {
+            return color;
+        }
+        var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+        var red = parseInt(digits[2]);
+        var green = parseInt(digits[3]);
+        var blue = parseInt(digits[4]);
+        var rgb = blue | (green << 8) | (red << 16);
+        return digits[1] + '#' + rgb.toString(16);
     }
     $window.on('load', initCustomizer);
 }(jQuery));
