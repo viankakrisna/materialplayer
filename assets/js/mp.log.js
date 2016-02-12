f096b86785ad9a1d1d303ca216412be636af3d4f
Ade Viankakrisna <viankakrisna@gmail.com>
2016-02-13 02:36:26 +0700

    create phonegap branch

--- assets/js/mp.log.js
+++
@@ -1,9 +0,0 @@
-window.MP = window.MP || {};
-MP.log = (function ($) {
-    return function () {
-        var args = [].slice.call(arguments);
-        $('#log')
-            .html(args.join);
-    }
-}(jQuery));
-
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var concat = require('gulp-concat');
-
-gulp.task('uglify', function() {
-    return gulp.src('./assets/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist/'));
-});
